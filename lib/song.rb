6a8928599eecdc52b5424758f3a82cb2c1e222eb
Chayan Martinez <nayachmartinez68@gmail.com>
2020-02-14 04:44:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
   
   def self.new_by_filename(file)
     song_name = file.split(" - ")[1]
-    artistname = file.split(" - ")[0]
+    artist = file.split(" - ")[0]
     song = self.new(song_name)
-    song.artist_name= artistname
+    song.artist_name= artist
     song
   
   def artist_name=(name) # helper method for self.new_by_filename
