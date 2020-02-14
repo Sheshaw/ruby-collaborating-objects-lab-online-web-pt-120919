cbb150b29b366ec281c1f5d20884b9c626ee598c
Chayan Martinez <nayachmartinez68@gmail.com>
2020-02-14 04:41:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,6 +20,11 @@ class Song
     # song = self.new(song_name)
     # song.artist_name= artistname
     # song
+    def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
   end
   
   def artist_name=(name) # helper method for self.new_by_filename
