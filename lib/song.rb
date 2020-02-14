7477c08c1a38b9cde359c4aa4fae2b500d16dcf6
Chayan Martinez <nayachmartinez68@gmail.com>
2020-02-14 04:00:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
     @@all 
   end
   
-  def self.new_by_filename (file)
+  def self.new_by_filename(file)
     song_name = file.split("-")[1]
     artist= file.split ("-")[0]
     song= self.new (song_name)
