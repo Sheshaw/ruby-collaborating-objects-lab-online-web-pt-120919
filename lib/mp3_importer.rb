e42de776825b5cbd9f29aaf533ce9f3ea6fa8d21
Chayan Martinez <nayachmartinez68@gmail.com>
2020-02-14 04:29:39 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -19,7 +19,7 @@ class MP3Importer
   end
   
   def import # imports the files into the library by creating songs from a filename
-    self.files.each {|filename| Song.new_by_filename(filename)}
+    files.each {|filename| Song.new_by_filename(filename)}
   end
   
 end
