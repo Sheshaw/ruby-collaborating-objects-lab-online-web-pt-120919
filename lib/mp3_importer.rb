0b9900d508118fd02954e1c0392b854d12b2fd72
Chayan Martinez <nayachmartinez68@gmail.com>
2020-02-14 03:58:17 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,11 +14,11 @@ class MP3Importer
     @path = path
   end
   
-  def files
+  def files # loads all the mp3 files in the path directory and normalizes the filename   # to just the mp3 filename with no path
     Dir.entries(path).select {|entry| entry.include?(".mp3")}
   end
   
-  def import
+  def import # imports the files into the library by creating songs from a filename
     files.each {|filename| Song.new_by_filename(filename)}
   end
   
